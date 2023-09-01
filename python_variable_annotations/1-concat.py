0f6bd3ec631ba45f5cdc216b5c2e734a6850c6ed
Siwarayari <3787@holbertonstudents.com>
2023-09-01 18:48:05 +0000

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""concat that takes a string a and  string
+b as arguments and returns their concat as a string."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concat of two strings"""
+    return(str1 + str2)
