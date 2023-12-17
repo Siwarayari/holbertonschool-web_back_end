a39d331063a4fc17427817a2e17cf4fa2ef7364a
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-12-17 15:57:27 +0100

    2-app.py

---
+++ i18n/2-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel, refresh, request
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route('/')
+def Welcome():
+    """basic Flask app"""
+    return render_template('0-index.html')
+
+
+class Config(object):
+    """Supported languages list"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """function with the babel.localeselector decorator.
+    Use request.accept_languages to determine the best
+    match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
