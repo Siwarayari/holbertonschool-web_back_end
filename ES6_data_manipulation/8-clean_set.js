c7f06877d55d16a97f241bf70be7dd13499d5389
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2024-01-06 20:19:34 +0100

    task8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string') {
-    return '';
-  }
-  return 'jovi-aparte-appetit';
+  if (typeof startString !== 'string') { return ''; }
+  if (typeof set !== typeof (new Set())) { return ''; }
+  if (startString === '') { return ''; }
+  const result = [];
+  set.forEach((element) => {
+    if (element && element.startsWith(startString) && startString !== '') {
+      result.push(element.slice(startString.length, element.length));
+    }
+  });
+  return result.join('-');
 }
