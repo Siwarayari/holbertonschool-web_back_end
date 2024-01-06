4445d4c427c5dd03925d5121c56c457cf77c5731
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2024-01-06 21:05:54 +0100

    task0

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+
+const calculateNumber = require("./0-calcul.js");
+
+
+  describe('sum', () => {
+    it(`...`, () => {
+      assert.strictEqual(calculateNumber(1, 3), 4);
+      });
+      it(`...`, () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+      });
+      it(`...`, () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+      });
+      it(`...`, () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      });
+  });
